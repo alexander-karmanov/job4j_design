882d4f32afa9c10ed69281e814e7f3ab2e28e124
alexander-karmanov <lektor_md@mail.ru>
2023-02-15 21:45:08 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,72 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements LinkedList<E>  {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    private Node<E> tail;
+
+    @Override
+    public void add(E value) {
+        final Node<E> l = tail;
+        final Node<E> newNode = new Node<>(value, head);
+        tail = newNode;
+            if (l == null) {
+                head = newNode;
+            } else {
+                l.next = newNode;
+            }
+        modCount++;
+        size++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> rsl = head;
+            for (int i = 0; i < index; i++) {
+                rsl = rsl.next;
+            }
+        return rsl.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            final int expectedModCount = modCount;
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E current = head.item;
+                head = head.next;
+                index++;
+                return current;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        final E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
