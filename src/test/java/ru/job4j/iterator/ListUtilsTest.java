cd833786c59e547e3c2b29ad38245cf9baec6d49
alexander-karmanov <lektor_md@mail.ru>
2023-02-27 22:06:14 +0300

    7. ListIterator [#350217]

---
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtilsTest {
+    private List<Integer> input;
+
+    @BeforeEach
+    void setUp() {
+        input = new ArrayList<>(Arrays.asList(1, 3));
+    }
+
+    @Test
+    void whenAddBefore() {
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddAfter() {
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddAfterWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addAfter(input, 2, 5))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenRemoveIfMoreThanTwo() {
+        ListUtils.addAfter(input, 0, 2);
+        Predicate<Integer> predicate = (s) -> s > 2;
+        ListUtils.removeIf(input, predicate);
+        assertThat(input).hasSize(2).containsSequence(1, 2);
+    }
+
+    @Test
+    void whenReplaceIfMoreThanTwo() {
+        ListUtils.addAfter(input, 0, 2);
+        ListUtils.addAfter(input, 1, 3);
+        Predicate<Integer> predicate = (s) -> s > 2;
+        ListUtils.replaceIf(input, predicate, 0);
+        assertThat(input).hasSize(4).containsSequence(1, 2, 0, 0);
+    }
+
+    @Test
+    void whenReplaceAll() {
+        ListUtils.addAfter(input, 0, 2);
+        List<Integer> elements = new ArrayList<>(Arrays.asList(1, 3, 5, 7));
+        ListUtils.removeAll(input, elements);
+        assertThat(input).hasSize(1).containsSequence(2);
+    }
+}
