0c5b68e2b2c938769495b98eba7aa5d67753b919
alexander-karmanov <lektor_md@mail.ru>
2023-05-06 17:33:33 +0300

    7. Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            String flag = null;
+            while (reader.ready()) {
+                String status = reader.readLine();
+                if (flag == null && (status.startsWith("400") || status.startsWith("500"))) {
+                    writer.write(status.split(" ")[1] + ";");
+                    flag = status;
+                } else if (flag != null && (!status.startsWith("400") || !status.startsWith("500"))) {
+                    writer.write(status.split(" ")[1] + ";");
+                    flag = null;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
