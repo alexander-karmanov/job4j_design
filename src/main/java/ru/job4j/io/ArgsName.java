f78bacca3878255333653949b8cfef944f73d77b
alexander-karmanov <lektor_md@mail.ru>
2023-05-21 13:17:29 +0300

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,58 @@
+package ru.job4j.io;
+
+import java.util.*;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+        }
+        return values.get(key);
+    }
+
+    private static void validation(String[] args) {
+
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+        for (String arg : args) {
+            if (!arg.startsWith("-")) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not start with a '-' character");
+            }
+            if (arg.startsWith("-=")) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a key");
+            }
+            if (arg.endsWith("=")) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a value");
+            }
+             if (!arg.contains("=")) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain an equal sign");
+            }
+        }
+    }
+    private void parse(String[] args) {
+        for (String arg: args) {
+            arg = arg.replaceFirst("-", "");
+            String[] split = arg.split("=");
+            values.put(split[0], split[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        validation(args);
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+
+        ArgsName jvm = ArgsName.of(new String[] {"-request=?msg=Exit="});
+        System.out.println(jvm.get("request"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
