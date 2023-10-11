6b763a71da6011dbccf8c067191c6d31eaff3f92
alexander-karmanov <lektor_md@mail.ru>
2023-10-12 00:11:34 +0300

    2. Схема прав пользователи и роли. [#1093]

--- db/lesson_2_3_1_1093/create.sql
+++ db/lesson_2_3_1_1093/create.sql
@@ -1,11 +1,11 @@
 create table roles(
     id serial primary key,
-    role varchar(255),
+    role varchar(255)
 );
 
 create table users(
     id serial primary key,
-    name varchar(255)
+    name varchar(255),
     role_id int references roles(id)
 );
 
@@ -20,11 +20,21 @@ create table users(
      rule_id int references rules(id)
 );
 
+create table categories(
+    id serial primary key,
+    category varchar(255)
+);
+
+create table states(
+    id serial primary key,
+    state varchar(255)
+);
+
 create table items(
     id serial primary key,
     item varchar(255),
-    user_id int references users(id)
-    category_id int references categories(id)
+    user_id int references users(id),
+    category_id int references categories(id),
     state_id int references states(id)
 );
 
@@ -38,14 +48,4 @@ create table attachs(
     id serial primary key,
     attach varchar(255),
     item_id int references items(id)
-);
-
-create table categories(
-    id serial primary key,
-    category varchar(255),
-);
-
-create table states(
-    id serial primary key,
-    state varchar(255),
 );
