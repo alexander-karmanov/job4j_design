5d0868a3f4cc379ba3921398e53ed0bb819ed207
alexander-karmanov <lektor_md@mail.ru>
2023-10-11 21:28:55 +0300

    2. Схема прав пользователи и роли. [#1093]

--- db/lesson_2_3_1_1093/create.sql
+++ db/lesson_2_3_1_1093/create.sql
@@ -1,12 +1,12 @@
-create table users(
-    id serial primary key,
-    name varchar(255)
-);
-
 create table roles(
     id serial primary key,
     role varchar(255),
-    user_id int references users(id)
+);
+
+create table users(
+    id serial primary key,
+    name varchar(255)
+    role_id int references roles(id)
 );
 
  create table rules(
@@ -24,6 +24,8 @@ create table items(
     id serial primary key,
     item varchar(255),
     user_id int references users(id)
+    category_id int references categories(id)
+    state_id int references states(id)
 );
 
 create table comments(
@@ -41,11 +43,9 @@ create table attachs(
 create table categories(
     id serial primary key,
     category varchar(255),
-    item_id int references items(id)
 );
 
 create table states(
     id serial primary key,
     state varchar(255),
-    item_id int references items(id)
 );
--- db/lesson_2_3_1_1093/insert.sql
+++ db/lesson_2_3_1_1093/insert.sql
@@ -1,8 +1,8 @@
-insert into users(name) values ('Jason');
-insert into users(name) values ('Robert');
+insert into roles (role) values ('admin');
+insert into roles (role) values ('user');
 
-insert into roles (role, user_id) values ('admin', 1);
-insert into roles (role, user_id) values ('user', 2);
+insert into users(name, role_id) values ('Jason', 1);
+insert into users(name, role_id) values ('Robert', 2);
 
 insert into rules (rule) values ('add item');
 insert into rules (rule) values ('edit item');
@@ -14,10 +14,10 @@ insert into roles_rules(role_id, rule_id) values (1, 4);
 insert into roles_rules(role_id, rule_id) values (2, 2);
 insert into roles_rules(role_id, rule_id) values (2, 3);
 
-insert into items (item, user_id) values ('first item', 1);
-insert into items (item, user_id) values ('second item', 1);
-insert into items (item, user_id) values ('third item', 2);
-insert into items (item, user_id) values ('fourth item', 2);
+insert into items (item, user_id, category_id, state_id) values ('first item', 1, 1, 1);
+insert into items (item, user_id, category_id, state_id) values ('second item', 1, 2, 2);
+insert into items (item, user_id, category_id, state_id) values ('third item', 2, 3, 3);
+insert into items (item, user_id, category_id, state_id) values ('fourth item', 2, 4, 4);
 
 insert into comments (comment, item_id) values ('Decent bike for this price', 1);
 insert into comments (comment, item_id) values ('the bike came packaged ok', 2);
