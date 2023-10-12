d691f6669b0a12e0c9a91cfe560b175548d37834
alexander-karmanov <lektor_md@mail.ru>
2023-10-12 12:58:35 +0300

    2. Схема прав пользователи и роли. [#1093]

--- db/lesson_2_3_1_1093/insert.sql
+++ db/lesson_2_3_1_1093/insert.sql
@@ -14,25 +14,25 @@ insert into roles_rules(role_id, rule_id) values (1, 4);
 insert into roles_rules(role_id, rule_id) values (2, 2);
 insert into roles_rules(role_id, rule_id) values (2, 3);
 
+insert into categories (category) values ('sports');
+insert into categories (category) values ('electronics');
+insert into categories (category) values ('books');
+insert into categories (category) values ('art');
+
+insert into states(state) values ('confirmed');
+insert into states(state) values ('shipped');
+insert into states(state) values ('on the way');
+insert into states(state) values ('delivered');
+
 insert into items (item, user_id, category_id, state_id) values ('first item', 1, 1, 1);
 insert into items (item, user_id, category_id, state_id) values ('second item', 1, 2, 2);
 insert into items (item, user_id, category_id, state_id) values ('third item', 2, 3, 3);
 insert into items (item, user_id, category_id, state_id) values ('fourth item', 2, 4, 4);
 
-insert into comments (comment, item_id) values ('Decent bike for this price', 1);
-insert into comments (comment, item_id) values ('the bike came packaged ok', 2);
-insert into comments (comment, item_id) values ('Nice looking bike. Surprisingly light!', 3);
-insert into comments (comment, item_id) values ('What is in the picture is not what you get.', 4);
+insert into comments (comment, item_id) values ('Decent bike for this price', 4);
+insert into comments (comment, item_id) values ('the bike came packaged ok', 5);
+insert into comments (comment, item_id) values ('Nice looking bike. Surprisingly light!', 6);
+insert into comments (comment, item_id) values ('What is in the picture is not what you get.', 7);
 
-insert into attachs (attach, item_id) values ('file1.jpg', 1);
-insert into attachs (attach, item_id) values ('file2.jpg', 2);
-
-insert into categories (category, item_id) values ('sports', 1);
-insert into categories (category, item_id) values ('electronics', 2);
-insert into categories (category, item_id) values ('books', 3);
-insert into categories (category, item_id) values ('art', 4);
-
-insert into states(state, item_id) values ('confirmed', 1);
-insert into states(state, item_id) values ('shipped', 2);
-insert into states(state, item_id) values ('on the way', 3);
-insert into states(state, item_id) values ('delivered', 4);
+insert into attachs (attach, item_id) values ('file1.jpg', 4);
+insert into attachs (attach, item_id) values ('file2.jpg', 5);
