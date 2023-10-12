8b5c6f111c48128687a8e76062d4e1f02cac1e72
alexander-karmanov <lektor_md@mail.ru>
2023-10-12 19:08:52 +0300

    0. Select [#6861]

---
+++ db/lesson_fauna_6861/insert.sql
@@ -0,0 +1,5 @@
+insert into fauna (name, avg_age, discovery_date) values ('catfish', 60, '1901-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('fox', 11, '1758-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('wolf', 10, null);
+insert into fauna (name, avg_age, discovery_date) values ('latimeria ', 25000, '1939-09-01');
+
