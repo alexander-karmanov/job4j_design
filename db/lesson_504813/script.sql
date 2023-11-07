f694350e8dc933e8b9128e2c475d7a1a72a83626
alexander-karmanov <lektor_md@mail.ru>
2023-11-07 21:30:51 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ db/lesson_504813/script.sql
@@ -0,0 +1,13 @@
+create table cars (
+    id serial primary key,
+    name varchar(25),
+    producer varchar(25),
+    count integer default 0,
+    price integer
+);
+
+insert into cars (name, producer, count, price) VALUES ('Audi A3', 'Audi', 5, 8300);
+insert into cars (name, producer, count, price) VALUES ('Hyundai Accent', 'Hyundai Motor Company', 20, 1500);
+insert into cars (name, producer, count, price) VALUES ('Kia Rio', 'Kia Motors', 17, 11600);
+
+
