12ca3d82e9af896b8cbc2b8564f139eb2a8749e9
alexander-karmanov <lektor_md@mail.ru>
2024-09-30 20:25:22 +0300

    3.2. Откат изменений [#504830]

--- src/main/java/ru/job4j/git/Task1.java
+++ src/main/java/ru/job4j/git/Task1.java
@@ -2,7 +2,7 @@ package ru.job4j.git;
 
 public class Task1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 30; i++) {
             System.out.println(i);
         }
     }
